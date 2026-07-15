a185670a8928bd5e0d70f28be1e77669f148cbde
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-15 23:44:22 +0530

    Sign in api completed

--- app.js
+++ app.js
@@ -12,6 +12,42 @@ app.use(Cors())
 
 Mongoose.connect("mongodb://aleena:aleena1234@ac-etp0lvq-shard-00-00.rb4mymb.mongodb.net:27017,ac-etp0lvq-shard-00-01.rb4mymb.mongodb.net:27017,ac-etp0lvq-shard-00-02.rb4mymb.mongodb.net:27017/blogappdb?ssl=true&replicaSet=atlas-mfk3xx-shard-0&authSource=admin&appName=Cluster0")
 
+//sign in
+
+app.post("/signIn",async(req,res)=>{
+
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+
+                const passwordValidator=Bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordValidator) {
+                    
+                    //pass token
+                    jwt.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},
+                        (error,token)=>{
+
+                            if (error) {
+                                res.json({"status":"error","errorMessage":error})
+                            } else {
+                                res.json({"status":"success","token":token,"userId":items[0]._id})
+                            }
+
+                    })
+
+                } else {
+                    res.json({"status":"invalid password"})
+                }
+            } else {
+                res.json({"status":"invalid email id"})
+            }
+        }
+    ).catch()
+
+})
+
+//signup
 app.post("/signup", async (req, res) => {
 
     let input = req.body
